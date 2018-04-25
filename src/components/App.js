1640e0a58bf633220d6c86218b34ec14e8484ef6
Eeps30 <eeps30@gmail.com>
2018-04-25 11:36:56 -0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import '../assets/css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Fire Chat</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
