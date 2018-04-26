c7d5c2ee60d03d66811cda68fda5366f536ca6ea
Eeps30 <eeps30@gmail.com>
2018-04-25 18:39:16 -0700

    finished sign in/out/up features

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
 import chat from './chat-reducer';
+import user from './user-reducer';
 
-export default combineReducers({ chat });
+export default combineReducers({ chat, form, user });
